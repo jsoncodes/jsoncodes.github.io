b7266ba259b87fd5ccd71df73ecefe040e8ba881
Jason <jasonmitchell@live.co.uk>
2016-02-09 20:51:40 +0000

    Adding fade in header

---
+++ scripts/layout.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  $(document).ready(function() {
+    var $header = $('.site-header');
+
+    if ($('.hero').length === 0) {
+      $header.addClass('solid');
+    } else {
+      $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        $header.toggleClass('solid', scroll >= 100);
+      });
+    }
+  });
+}());
