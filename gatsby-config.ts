6d6d5af12994ab6301e8d81994658c2fb191973b
Jason Mitchell <jasonmitchell@live.co.uk>
2023-02-02 21:28:19 +0000

    Enable google analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -56,7 +56,29 @@ const config: GatsbyConfig = {
     },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-Z57HXMCGTM", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
   ]
 };
 
